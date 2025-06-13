1b56d91a0bf071e61718a3697b33263237920eee
Renan Alves <renanalves23@outlook.com>
2025-06-13 17:22:12 -0300

    feat: mfe-cadastro update

--- mfe-cadastro/src/app/app.component.ts
+++ mfe-cadastro/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/services/user/user.service';
+import { UserModel } from 'src/models/user/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mfe-cadastro';
+
+  constructor(private fb: FormBuilder,
+     private userService: UserService,
+     private router: Router) {
+   
+  }
+
+  submit(user: UserModel): void {
+      this.userService.createUser(user).subscribe({
+        next: (createdUser) => {
+          this.handleDispatch(createdUser);
+          this.router.navigate(['/sucesso']);
+        },
+        error: (err) => console.error('Error creating user:', err),
+      });
+    
+  }
+  
+  handleDispatch(user: UserModel): void {
+    window.sharedDataService.setUser(user);
+  }
+
+  handleUserForm(user: UserModel): void {
+    this.submit(user);
+  }
+
 }
