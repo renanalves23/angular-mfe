db2a7d1b8c4f0743448a74c51b6c1e415f92662b
Renan Alves <renanalves23@outlook.com>
2025-06-14 10:40:46 -0300

    feat(register): environments

--- mfe-cadastro/src/services/user/user.service.ts
+++ mfe-cadastro/src/services/user/user.service.ts
@@ -1,21 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { UserModel } from 'src/models/user/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private readonly API_URL = 'http://localhost:3000/users';
+  private readonly BASE_API_URL = environment.BASE_API_URL;
 
   constructor(private http: HttpClient) {}
 
   createUser(user: UserModel): Observable<UserModel> {
-    return this.http.post<UserModel>(this.API_URL, user);
+    return this.http.post<UserModel>(`${this.BASE_API_URL}/users`, user);
   }
 
   getUsers(): Observable<UserModel[]> {
-    return this.http.get<UserModel[]>(this.API_URL);
+    return this.http.get<UserModel[]>(`${this.BASE_API_URL}/users`);
   }
 }
